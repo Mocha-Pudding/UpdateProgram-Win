4006a8f00036a446edac5669a4e90c27c95806a9
Mocha-Pudding <zhujiangge6@126.com>
2018-07-19 22:02:46 +0800

    UpdateProgram-Win

---
+++ UpdateProgram-Win/root/firstsite/firstapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, HttpResponse
+from firstapp.models import People, Article
+from django.template import Context, Template
+
+# Create your views here.
+def first_try(request):
+    person = People(name='Charles', job='developer')
+    html_string = '''
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css" media="screen" title="no title">
+                <title>firstapp</title>
+            </head>
+            <body>
+                <h1 class="ui center aligned icon heeader">
+                    <i class="hand spock icon"></i>
+                    Hello, {{person.name}}
+                </h1>
+            </body>
+        </html>
+
+    '''
+
+    t = Template(html_string)
+    c = Context({'person':person})
+    web_page = t.render(c)
+    return HttpResponse(web_page)
+
+def index(request):
+    context = {}
+    article_list = Article.objects.all()
+    context['article_list'] = article_list
+    index_page = render(request, 'first_web_2.html', context)
+    return index_page
